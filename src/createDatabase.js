54a3c4b1d0a08c49a5267be64aad92b1fb174606
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-07-27 16:46:21 +0530

    updated index.js

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
-
 const mongoose = require('mongoose');
 const subscriberModel = require('./models/subscribers');
 const data = require('./data');
 
 // Connect to DATABASE
-const DATABASE_URL = process.env.DATABASE_URI; //* used mongoDB driver
+const DATABASE_URL =
+  'mongodb+srv://yatharthshahrawat97:HTWHMq5mj1tbEd9P@nodeexpressprojects.6n4kl6k.mongodb.net/?retryWrites=true&w=majority'; //* used mongoDB driver
 mongoose.connect(DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
