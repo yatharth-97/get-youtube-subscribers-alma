33ee71dabd12e27ae3d44e4c0c267607ab0b2fbc
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-07-29 01:01:35 +0530

    updated cutom-error.js

--- src/error/custom-error.js
+++ src/error/custom-error.js
@@ -1,12 +1,11 @@
-// this custom api class will help us in throwing different errors along
-// with their statuscodes when used.
 class CustomAPIError extends Error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
   }
 }
-
+// this custom api class will help us in throwing different errors along
+// with their statuscodes when used.
 const createCustomError = (msg, statusCode) => {
   return new CustomAPIError(msg, statusCode);
 };
