e1c0ebdb7c20e6ccd47c5c8b149f21e4d8a411ad
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-07-28 15:01:00 +0530

    updated URI

--- src/controllers/subscribersController.js
+++ src/controllers/subscribersController.js
@@ -31,54 +31,8 @@ const getSubscribersByID = async (req, res) => {
   res.status(200).json({ subscriber });
 };
 
-//---------------------------------EXTRA CONTENT-----------------------------------------------
-// posts new subscriber's name and the channel to which he is subscribed
-// adding new content to the database api
-// data is sent to body.create
-const postSubscribers = async (req, res) => {
-  const subscriber = new Subscriber({
-    name: req.body.name,
-    subscribedChannel: req.body.subscribedChannel,
-  });
-  try {
-    const newSubscriber = await subscriber.save();
-    res.status(201).json(newSubscriber);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
-
-// updates name and subscribedChannel info in body
-const patchSubscribers = async (req, res) => {
-  if (req.body.name != null) {
-    res.subscriber.name = req.body.name;
-  }
-  if (req.body.subscribedChannel != null) {
-    res.subscriber.subscribedChannel = req.body.subscribedChannel;
-  }
-  try {
-    const updatedSubscriber = await res.subscriber.save();
-    res.json(updatedSubscriber);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
-
-// deletes the subscriber from the database
-const deleteSubscribers = async (req, res) => {
-  try {
-    await res.subscriber.deleteOne();
-    res.json({ message: 'SUBSCRIBER DELETED' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
 module.exports = {
   getSubscribers,
   getSubscribersNames,
   getSubscribersByID,
-  postSubscribers,
-  patchSubscribers,
-  deleteSubscribers,
 };
