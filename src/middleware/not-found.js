b713eba43c3d7a185f050f6a3d287089ff1a5593
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-07-28 22:15:37 +0530

    updated middleware

--- src/middleware/not-found.js
+++ src/middleware/not-found.js
@@ -1,4 +1,6 @@
 const notFound = (req, res) =>
-  res.status(404).json({ msg: 'Route does not exist. Type again.' });
+  res
+    .status(404)
+    .json({ msg: 'Route does not exist. Please enter subscribers route.' });
 
 module.exports = notFound;
